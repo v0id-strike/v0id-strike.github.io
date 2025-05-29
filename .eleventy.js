21675f9bbdad6e9032d483dee9631fb94313a7ab
Void Strike <kaydzyu1508@gmail.com>
2025-05-30 02:09:27 +0500

    ordering module

--- .eleventy.js
+++ .eleventy.js
@@ -19,7 +19,7 @@ module.exports = function(eleventyConfig) {
         return collection.getFilteredByGlob("src/posts/**/*.md");
     });
 
-        // Add getCategories filter
+    // Add getCategories filter
     eleventyConfig.addFilter("getCategories", function(collections) {
         const categories = new Set();
         
@@ -36,10 +36,29 @@ module.exports = function(eleventyConfig) {
         return Array.from(categories).sort();
     });
 
+    // Custom filter to sort by order
+    eleventyConfig.addFilter("sortByOrder", function(collection) {
+        return collection.sort((a, b) => {
+            // Get order values, default to 999 if not set
+            const orderA = a.data.order || 999;
+            const orderB = b.data.order || 999;
+            
+            // First sort by order
+            if (orderA !== orderB) {
+                return orderA - orderB;
+            }
+            
+            // If orders are equal, sort by date
+            return new Date(b.date) - new Date(a.date);
+        });
+    });
+
     return {
         dir: {
             input: "src",
-            output: "docs"
+            output: "_site",
+            includes: "_includes",
+            layouts: "_includes/layouts"
         }
     };
 };
